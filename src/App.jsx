fcd2a6a48486a74ad7470b0e4837e0dd7add62cd
fitoji <fitoji@gmail.com>
2024-11-23 19:08:56 +0100

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Footer from './components/Footer'
 //import MNAForm from './components/MNAForm'
 
 import ModernMNAForm from './components/ModernMNAForm'
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="mx-auto p-4 bg-emerald-100">
       <ModernMNAForm />
+      <Footer/>
     </div>
   )
 }
